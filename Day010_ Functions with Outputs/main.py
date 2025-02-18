87b3c8f9201791a924d5e76cb0463a7b920a3c1c
Abdalr7man <bdwmhmd416@gmail.com>
2025-02-18 03:30:32 +0200

    ---

--- Day010_ Functions with Outputs/main.py
+++ Day010_ Functions with Outputs/main.py
@@ -7,7 +7,7 @@ def num1fun():
         if num1.isdigit():
             num1=int(num1)
             break
-        else : print("Valid input.")
+        print("Valid input.")
 
 def operationfun():
     global operation
@@ -15,7 +15,7 @@ def operationfun():
         operation=input("+\n-\n*\n/\nPick an operation: ")
         if operation in ["+","-","*","/"]:
             return operation
-        else : print("Valid input.")
+        print("Valid input.")
 
 def num2fun():
     global num2
@@ -24,23 +24,18 @@ def num2fun():
         if num2.isdigit():
             num2=int(num2)
             break
-        else : print("Valid input.")
-
+        print("Valid input.")
 
 def add(num1,num2):
-    print(f"{num1} {operation} {num2} = {num1+num2}")
     return num1+num2
 
 def substract(num1,num2):
-    print(f"{num1} {operation} {num2} = {num1-num2}")
     return num1-num2
 
 def multiply(num1,num2):
-    print(f"{num1} {operation} {num2} = {num1*num2}")
     return num1*num2
 
 def divide(num1,num2):
-    print(f"{num1} {operation} {num2} = {num1/num2}")
     return num1/num2
 
 dict_operations={"*":multiply,"/":divide,"+":add,"-":substract}
@@ -48,9 +43,10 @@ dict_operations={"*":multiply,"/":divide,"+":add,"-":substract}
 print(logo)
 num1fun()
 while True :
-    print(logo)
-    operationfun()
-    num2fun()
+    print(logo) 
+    operationfun()   # TODO : Handling ZeroDivisionError 
+    num2fun()  
+    print(f"{num1} {operation} {num2} = {dict_operations[operation](num1,num2)}")
     cont=input(f"Type 'y' to continue calculating with {dict_operations[operation](num1,num2)}, or type 'n' to start a new calculation: ").lower()
     if cont =="y":
         num1=dict_operations[operation](num1,num2)
