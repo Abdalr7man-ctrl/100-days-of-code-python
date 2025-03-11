a7a30c7691e9bf91d422dbe1d7690267307c86ea
Abdalr7man <bdwmhmd416@gmail.com>
2025-03-11 14:24:49 +0200

    date

--- Day012_Scope&Number-Guessing/art.py
+++ Day012_Scope&Number-Guessing/art.py
@@ -1,9 +1,3 @@
 import pyfiglet
 
 logo = pyfiglet.figlet_format("Guess The Number", font = "slant", width = 100, )
-
-
-if __name__ == "__main__" :
-    print(logo)
-
-
--- Day012_Scope&Number-Guessing/main.py
+++ Day012_Scope&Number-Guessing/main.py
@@ -1,57 +1,44 @@
 import random
 from art import logo
 
-def game():
+def get_guess():
+    global guess 
+    guess = input("Make a guess: ")
+    while not guess.isdigit() :
+        guess = input("Make a guess: ")
 
+def intro():
     print(logo)
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-    num = random.randrange(0,101) 
+    num = random.randrange(1,101) 
+    chance = 10 if difficulty == "easy" else 5
+    return chance, num
 
-    if difficulty == "easy" :
-        
-        chance = 10
-        
-        while chance > 0 :
-            print(f"You have {chance} attempts remaining to guess the number.")
-            guess = input("Make a guess: ")
-            while not guess.isdigit() :
-                guess = input("Make a guess: ")
-            
-            if int(guess) == num :
-                print(f"you got it the number is {num}")
-                break
-            else :
-                chance -= 1
-                if num > int(guess) :
-                    print("Too low")
-                else :
-                    print("Too hight")
+def game():
+    chance, num = intro()
+    while chance > 0 :
+        print(f"You have {chance} attempts remaining to guess the number.")
+        get_guess()
+        if int(guess) == num :
+            print(f"you got it the number is {num}")
+            break
+        elif int(guess) not in range(1,101) :
+            print("Out of range 1->100 Try again.")
         else :
-            print("You've run out of guesses. Refresh the page to run again.")
-
+            chance -= 1
+            if num > int(guess) :
+                print("Too low")
+            else :
+                print("Too high")
     else :
+        print("You've run out of guesses Game Over.")
 
-        chance = 5
-        
-        while chance > 0 :
-            print(f"You have {chance} attempts remaining to guess the number.")
-            guess = input("Make a guess: ")
-            while not guess.isdigit() :
-                guess = input("Make a guess: ")
-            
-            if int(guess) == num :
-                print(f"you got it the number is {num}")
-                break
-            else :
-                chance -= 1
-                if num > int(guess) :
-                    print("Too low")
-                else :
-                    print("Too hight")
-        else :
-            print("You've run out of guesses. Refresh the page to run again.")
-
-
-game()
+while True :
+    game()
+    again = input("would you want to play again? y or n ")
+    if again.lower() == "y" :
+        continue
+    else :
+        break
